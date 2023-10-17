22502d9f5c416940099168e7c985a2546819607c
mohamedBoutaieb <mohamedboutaieb1459@gmail.com>
2023-10-17 19:11:17 +0100

    IT

---
+++ src/test/java/com/mycompany/applicationone/web/webRequestsTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.applicationone.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.emptyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class webRequestsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testTheEndpount() throws Exception {
+        mockMvc.perform(get("/api/v1/welcome"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome to the world of Azure Containers!"));
+    }
+}
