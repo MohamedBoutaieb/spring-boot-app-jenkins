6fd7497c8ecd6f5d4e59e0ac380ab348414f8df6
mohamedBoutaieb <mohamed@letsfluence.com>
2023-11-29 15:04:44 +0100

    feat : kubernetes script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
     region ="us-east-1"
-  
 }
 
 resource "aws_instance" "ec2_for_springboot" {
@@ -10,4 +9,25 @@ resource "aws_instance" "ec2_for_springboot" {
         Name = "ec2_for_springboot"}
 }
 
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com",
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_admin_attachment" {
+  name       = "eks-cluster-admin-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  roles      = [aws_iam_role.eks_cluster.name]
+}
